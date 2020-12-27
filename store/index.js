ea3986fa8682609e44022f5176ae9101b9ceaf92
vshian <vvardhanreddy4@gmail.com>
2020-12-27 13:20:47 +0530

    added redux devtools

--- store/index.js
+++ store/index.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const configureStore = (initialState = {}) => {
   const middleware = [thunk];
+
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(...middleware)
+    composeWithDevTools(applyMiddleware(...middleware))
   );
 
   return store;
